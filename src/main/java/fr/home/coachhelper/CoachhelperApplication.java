fb1dcc5e19788330a72f192828955daa263992ca
matthieu bailly <matth.bailly@gmail.com>
2022-02-01 22:11:57 +0100

    init project

---
+++ src/main/java/fr/home/coachhelper/CoachhelperApplication.java
@@ -0,0 +1,13 @@
+package fr.home.coachhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachhelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/home/coachhelper/CoachhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.home.coachhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachhelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
